36514683e082c340c772a917a0a87b67188f7443
melkornwah <v.chistyi@gmail.com>
2023-04-18 12:14:46 +0300

    init git push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/gFRtAAmiFbE" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/Ou7nsYGn-KI" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/hibbOz6j2g8" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/ydYDqZQpim8" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/LrZnmXovp7E" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/PyNuRpZOtAY" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/j9O0FpVORck" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/iiD4OYBos1s" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
+      <div className="stream_container">
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/kzX0mO5Q_ME" title="YouTube video player"
+          frameBorder="0"
+          allow="autoplay; gyroscope; web-share"
+        />
+      </div>
     </div>
   );
 }
